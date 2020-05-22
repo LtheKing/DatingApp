37c281353873090e1ad06860a1965dcf68a65fed
Leonaldi Aldi <leonalding77@gmail.com>
2020-05-22 13:33:06 +0700

    end of section 12

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
@@ -10,5 +11,21 @@ namespace DatingApp.API.Dtos
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specified between 4 & 7 character")]
         public string Password { get; set; }
+        [Required]      
+        public string Gender { get; set; }
+        [Required]
+        public string KnownAs { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
